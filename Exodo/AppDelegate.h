a419582411f576fc662c270f637754f40c653728
Alvaro Franco <alvarofrancoayala@gmail.com>
2015-01-05 18:20:43 +0100

    Adds main Xcode project

---
+++ Exodo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Exodo
+//
+//  Created by Alvaro Franco on 05/01/15.
+//  Copyright (c) 2015 Exodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Exodo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exodo
+//
+//  Created by Alvaro Franco on 05/01/15.
+//  Copyright (c) 2015 Exodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
